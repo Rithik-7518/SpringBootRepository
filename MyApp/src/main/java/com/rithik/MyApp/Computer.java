53f592ee863b6214e512f997aa5f6ff73ad9e1f2
Rithik-7518 <rithikrosan7518@gmail.com>
2025-01-29 14:24:23 +0530

    Injection with SpringBoot

---
+++ MyApp/src/main/java/com/rithik/MyApp/Computer.java
@@ -0,0 +1,7 @@
+
+package com.rithik.MyApp;
+
+public interface Computer {
+
+    void compile();
+}
---
+++ MyApp/src/main/java/com/rithik/MyApp/Laptop.java
@@ -0,0 +1,10 @@
+package com.rithik.MyApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop implements Computer {
+    public void compile() {
+        System.out.println("Compiling with 404 bugs");
+    }
+}
