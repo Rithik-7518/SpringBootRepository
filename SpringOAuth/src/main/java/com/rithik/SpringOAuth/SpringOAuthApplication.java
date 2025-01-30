6bb7285283d163592e5b6a3b0d8a7b2b2ae8425a
Rithik-7518 <rithikrosan7518@gmail.com>
2025-01-30 21:57:30 +0530

    Sigin with Google and Github coding

---
+++ SpringOAuth/src/main/java/com/rithik/SpringOAuth/SpringOAuthApplication.java
@@ -0,0 +1,17 @@
+package com.rithik.SpringOAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOAuthApplication.class, args);
+		
+		//you must change the application.properties file
+		
+	}
+
+}
